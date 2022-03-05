df16bc7d53743108c61f10c6697af8aca8a263ae
Luis Bermudez <lapbermudez@gmail.com>
2022-03-04 22:35:54 -0600

    baseUrl setup - assign heroku's backend url for testing

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,8 @@
 import axios from "axios";
 
-const isProduction = process.env.NODE_ENV === "production";
-
-const baseURL = isProduction
-  ? "https://platybend.herokuapp.com/api"
-  : "http://localhost:5005/api";
+const baseURL =
+  "https://platybend.herokuapp.com/api";
+  // "http://localhost:5005/api";
 
 export const api = axios.create({
   baseURL,
