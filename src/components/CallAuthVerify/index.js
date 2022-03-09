4b55c67af16ceb853a3b719a3a576836d7277758
Luis Bermudez <lapbermudez@gmail.com>
2022-03-08 18:08:10 -0600

    Setup CallAuthVerify

--- src/components/CallAuthVerify/index.js
+++ src/components/CallAuthVerify/index.js
@@ -1,16 +1,12 @@
 import { authverifyProcess } from "../../redux/UserDuck";
 import { useDispatch } from "react-redux";
-import { useRef, useEffect } from "react";
+import { useEffect } from "react";
 
 const CallAuthVerify = ({ children }) => {
-  const hasVerified = useRef(false);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    // if (!hasVerified.current) {
       dispatch(authverifyProcess());
-      // hasVerified.current = true;
-    // }
   });
 
   return children;
--- src/components/IsPrivate/index.js
+++ src/components/IsPrivate/index.js
@@ -1,18 +1,14 @@
 import { authverifyProcess } from "../../redux/UserDuck";
 import { useDispatch, useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
-import { useRef, useEffect } from "react";
+import { useEffect } from "react";
 
 function IsPrivate({ children }) {
   const isLoggedIn = useSelector((state) => state.user.isLoggedIn);
-  const hasVerified = useRef(false);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    // if (!hasVerified.current) {
       dispatch(authverifyProcess());
-      // hasVerified.current = true;
-  //   }
   });
 
   if (!isLoggedIn) {
