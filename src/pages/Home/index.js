a483e42f424295a3e575171266e047e8546c3894
Luis Bermudez <lapbermudez@gmai.com>
2022-08-12 13:42:01 -0500

    Update fill-available to stretch - Remove window.reload() to update object intersector for now

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,16 +1,11 @@
-import { useEffect, useRef, useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { videolocationsCall } from "../../redux/videolocationSlice";
-import { Search } from "react-bootstrap-icons";
-import PreviewVideoCard from "../../components/PreviewVideoCard";
+import { useState } from "react";
+import { useSelector } from "react-redux";
 import "./Home.css";
 import placeholderPP from "../../Black-Dog-PNG.png";
 import VideoPlayer from "../../components/VideoPlayer";
 import VideoInfoHomeCard from "../../components/VideoInfoHomeCard";
-import $ from "jquery";
 
 function Home() {
-  const dispatch = useDispatch();
   const videolocations = useSelector(
     (state) => state.videolocation.videolocations
   );
@@ -22,21 +17,6 @@ function Home() {
     oneVideoPlaying,
     setOneVideoPlaying,
   ];
-
-  useEffect(() => {
-    dispatch(videolocationsCall());
-
-    $(window).on("resize", () => {
-      clearTimeout(window.resizedFinished);
-      window.resizedFinished = setTimeout(() => {
-        window.location.reload();
-      }, 250);
-    });
-
-    return () => {
-      $(window).off("resize");
-    };
-  }, []);
 
   return (
     <div
